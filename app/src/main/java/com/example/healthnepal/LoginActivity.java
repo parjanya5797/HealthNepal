acc3038259ea97226457f9999da8dc0f1a30d892
parjanya5797 <parjanya57@gmail.com>
2020-11-10 21:52:49 +0545

    Added Register Page

--- app/src/main/java/com/example/healthnepal/LoginActivity.java
+++ app/src/main/java/com/example/healthnepal/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.healthnepal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,5 +18,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.tv_register);
+       register.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+               startActivity(intent);
+           }
+       });
+
     }
 }
